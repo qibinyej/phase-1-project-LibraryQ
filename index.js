50202260b2ca214f554912ccd111805e929d3481
Qibin <qbye0405@gmail.com>
2022-06-01 21:57:49 -0400

    README updated

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
 //write your code here
-const navElement = document.querySelector('.book-list');
+fetch('https://openlibrary.org/subjects/picture_books.json?limit=100')
+.then(response => response.json())
+.then(data => {
+    displayBooks(data.works)
+    showBook(data.works[0])
+})
 
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
-// const bookCover = document.getElementById('cover-image');
-// const yearReleased = document.getElementById('year-released')
-// const isbn = document.getElementById('isbn');
-// const description = document.getElementById('description');
-
+const bookCover = document.getElementById('cover-image');
+const yearReleased = document.getElementById('year-released')
+const isbn = document.getElementById('isbn');
+const description = document.getElementById('description');
 const upvoteBtn = document.getElementById('upvote');
-
-fetch('https://openlibrary.org/subjects/picture_books.json')
-.then(response => response.json())
-.then(data => {    
-    console.log(data)
-    for(let item of data.works){
-        const list = document.createElement('li')
-        list.textContent = item.title;
-       
-        navElement.appendChild(list);
-
-        list.addEventListener('click', event => {
-            showBook(item)
-        })
-    }
-    showBook(data.works[0]);
-    })
-
-function showBook(bookInfo){
-   // bookCover.src = bookInfo.cover;
-    bookTitle.textContent = 'Title: ' + bookInfo.title;
-    bookAuthor.textContent = 'Author: ' + bookInfo.authors[0].name;
-   //yearReleased.textContent = bookInfo.publishDates;
-  // isbn.textContent = bookInfo.isbn;
-   // description.textContent = bookInfo.description;
-}
-
-//vote count incrementing by 1 ; need to get it to persist
-upvoteBtn.addEventListener('click', event => {
-    let voteCount = document.getElementById('vote-count')
-    voteCount = voteCount.textContent ++;
-})
-
-//add review on the bottom of the page
 const reviewForm = document.getElementById('review-form');
 const reviewBtn = document.getElementById('review');
+const feedback = document.getElementById('leaving-feedback')
+function displayBooks(data){
+    for(let item of data){
+        const header4 = document.createElement('h4')
+        header4.textContent = `${item.title}`;
+        const navElement = document.querySelector('.book-list')
+        navElement.appendChild(header4);
+        header4.addEventListener('click', () => {
+            showBook(item)
+        })
+    };
+};
+
+function showBook(bookInfo){
+    bookTitle.textContent = `Title: ${bookInfo.title}`;
+    bookAuthor.textContent = `Author: ${bookInfo.authors[0].name}`
+    bookCover.src = `https://covers.openlibrary.org/b/id/${bookInfo.cover_id}-L.jpg`
+}
+
+upvoteBtn.addEventListener('click', () => {
+    let voteCount = document.getElementById('vote-count');
+   voteCount=voteCount.textContent++;
+})
 
 reviewForm.addEventListener('submit', event => {
     event.preventDefault();
-    review.textContent = reviewBtn.value
+
+    let newReview = document.createElement('h3')
+    newReview.textContent = `“${reviewBtn.value}“`
+    document.querySelector('.review-container').appendChild(newReview);
     reviewForm.reset();
-    }
-)
-
-let books =[];
-
-const searchInput = document.querySelector('[data-search]')
-
-searchInput.addEventListener('input', event =>{
-    const value = event.target.value.toLowerCase;
-    books.forEach(book => {
-        const isVisible = book.title.includes(value) || book.authors.includes(value)
-        books.element.classlist.toggle('hide', !isVisible);
     })
+
+const searchInput = document.querySelector(['data-search']);
+
+console.log(searchInput)
+
+searchInput.addEventListener('input', e => {
+    const value = e.target.value;
+
 
 })
